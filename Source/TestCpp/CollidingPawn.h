b5d0d7ae4d124bb4920d8d803e8c9ddded6d0c2b
zhaohuappc <1970762994@qq.com>
2022-11-18 16:10:55 +0800

    添加按键控制移动  添加粒子特效组件以及绑定空格键的开关
    并且使用springarm 控制摄像机与 pawn角色的距离， pawn // 控制默认玩家
        AutoPossessPlayer = EAutoReceiveInput::Player0;  取代默认玩家镜头

---
+++ Source/TestCpp/CollidingPawn.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "CollidingPawn.generated.h"
+
+UCLASS()
+class TESTCPP_API ACollidingPawn : public APawn
+{
+    GENERATED_BODY()
+
+public:
+    // 设置此Pawn属性的默认值
+    ACollidingPawn();
+
+protected:
+    // 游戏开始或生成时调用
+    virtual void BeginPlay() override;
+
+public:
+    // 逐帧调用
+    virtual void Tick(float DeltaSeconds) override;
+
+    // 调用以将功能与输入绑定
+    virtual void SetupPlayerInputComponent(class UInputComponent* InInputComponent) override;
+
+    UPROPERTY(EditAnywhere)
+    class UParticleSystemComponent* OurParticleSystem;
+
+    UPROPERTY()
+    class UCollidingPawnMovementComponent* OurMovementComponent;
+
+    virtual UPawnMovementComponent* GetMovementComponent() const override;
+
+    void MoveForward(float AxisValue);
+    void MoveRight(float AxisValue);
+    void Turn(float AxisValue);
+    void ParticleToggle();
+};
