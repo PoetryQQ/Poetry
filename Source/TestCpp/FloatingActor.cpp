dff416fb5c7bb0d5dea2913b7256ae4d30c55f5d
zhaohuappc <1970762994@qq.com>
2022-11-18 14:20:41 +0800

    添加  摄像机代码切换

---
+++ Source/TestCpp/FloatingActor.cpp
@@ -0,0 +1,43 @@
+// 版权所有 1998-2019 Epic Games, Inc。保留所有权利。
+
+#include "FloatingActor.h"
+
+// 设置默认值
+AFloatingActor::AFloatingActor()
+{
+    // 将此Actor设为逐帧调用Tick()。如无需此功能，可关闭以提高性能。
+    PrimaryActorTick.bCanEverTick = true;
+
+    VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+    VisualMesh->SetupAttachment(RootComponent);
+
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> CubeVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube"));
+
+    if (CubeVisualAsset.Succeeded())
+    {
+        VisualMesh->SetStaticMesh(CubeVisualAsset.Object);
+        VisualMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+    }
+}
+
+// 游戏开始时或生成时调用
+void AFloatingActor::BeginPlay()
+{
+    Super::BeginPlay();
+
+}
+
+// 逐帧调用
+void AFloatingActor::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    FVector NewLocation = GetActorLocation();
+    FRotator NewRotation = GetActorRotation();
+    float RunningTime = GetGameTimeSinceCreation();
+    float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
+    NewLocation.Z += DeltaHeight * 20.0f;       //Scale our height by a factor of 20
+    float DeltaRotation = DeltaTime * 20.0f;    //Rotate by 20 degrees per second
+    NewRotation.Yaw += DeltaRotation;
+    SetActorLocationAndRotation(NewLocation, NewRotation);
+}
